850f626d7339ca992552b31afeb4e5fe9db5027c
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-01-08 18:02:32 +0900

    feat:test

--- src/store_sales/store_sales.rs
+++ src/store_sales/store_sales.rs
@@ -1,7 +1,6 @@
 use plotters::prelude::*;
 use linfa::prelude::*;
-use std::{fs::File, cmp::Ordering};
-use polars::{prelude::*, series::SeriesTrait};
+use polars::prelude::*;
 use chrono::{Utc, TimeZone};
 
 
@@ -57,7 +56,77 @@ pub fn main(){
         )
     ).unwrap();
 
-
+    //store_nbr
+    let store_nbr_data= train_df.select(["store_nbr","sales"]).unwrap();
+     let store_nbr_data= store_nbr_data.to_ndarray::<Float64Type>(IndexOrder::Fortran).unwrap();
+     let mut store_nbr_data_vec: Vec<Vec<_>> = Vec::new();
+     for row in store_nbr_data.outer_iter() {
+         let row_vec: Vec<_> = row.iter().cloned().collect();
+         store_nbr_data_vec.push(row_vec);
+     }
+    
+     let store_nbr_root = BitMapBackend::new("./src/store_sales/store_nbr.png", (800, 600)).into_drawing_area();
+     store_nbr_root.fill(&WHITE).unwrap();
+  
+  
+         let mut store_nbr_chart: ChartContext<'_, BitMapBackend<'_>, Cartesian2d<plotters::coord::types::RangedCoordf64, plotters::coord::types::RangedCoordf64>> = ChartBuilder::on(&store_nbr_root)
+         .margin(20)
+         .x_label_area_size(40)
+         .y_label_area_size(40)
+         .build_cartesian_2d((0.0..60.0), (0.0..12500.0))
+         .unwrap();
+ 
+         store_nbr_chart
+         .configure_mesh()
+         .x_desc("Length")
+         .y_desc("Weight")
+         .draw()
+         .unwrap();
+        store_nbr_chart.draw_series(
+            LineSeries::new(
+                store_nbr_data_vec.iter().map(|x| {                
+                    (x[0], x[1])
+                }),
+                &BLUE,
+            )
+        ).unwrap();
+    
+     //store_nbr
+     let onpromotion_data= train_df.select(["onpromotion","sales"]).unwrap();
+     let onpromotion_data= onpromotion_data.to_ndarray::<Float64Type>(IndexOrder::Fortran).unwrap();
+     let mut onpromotion_data_vec: Vec<Vec<_>> = Vec::new();
+     for row in onpromotion_data.outer_iter() {
+         let row_vec: Vec<_> = row.iter().cloned().collect();
+         onpromotion_data_vec.push(row_vec);
+     }
+    
+     let onpromotion_root = BitMapBackend::new("./src/store_sales/onpromotion.png", (800, 600)).into_drawing_area();
+     onpromotion_root.fill(&WHITE).unwrap();
+  
+  
+         let mut onpromotion_chart: ChartContext<'_, BitMapBackend<'_>, Cartesian2d<plotters::coord::types::RangedCoordf64, plotters::coord::types::RangedCoordf64>> = ChartBuilder::on(&onpromotion_root)
+         .margin(20)
+         .x_label_area_size(40)
+         .y_label_area_size(40)
+         .build_cartesian_2d((0.0..1000.0), (0.0..125000.0))
+         .unwrap();
+ 
+         onpromotion_chart
+         .configure_mesh()
+         .x_desc("Length")
+         .y_desc("Weight")
+         .draw()
+         .unwrap();
+        onpromotion_chart.draw_series(
+            LineSeries::new(
+                onpromotion_data_vec.iter().map(|x| {                
+                    (x[0], x[1])
+                }),
+                &BLUE,
+            )
+        ).unwrap();
+    
+    
     // let date_data= date_data.to_ndarray::<FromDataUtf8>(IndexOrder::Fortran).unwrap();
     /*===================visualization========================= */
 
