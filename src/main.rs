709f19fdcdcd400050beb5531d665f6c325ef9ab
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-01-14 17:57:14 +0900

    feat:icr_identifying_age_related_conditions

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod price_prediction_for_used_cars;
 mod predict_co2_emissions_in_rwanda;
 mod forecasting_mini_cource_sales;
 mod data_science_london;
+mod icr_identifying_age_related_conditions;
 fn main() {
     // titanic::titanic::main();
     // price_prediction_for_used_cars::model::main();
@@ -16,5 +17,7 @@ fn main() {
     // binary_classification_with_a_bank_churn_dataset::model::main();
     // predict_co2_emissions_in_rwanda::model::main();
     // forecasting_mini_cource_sales::model::main();
-    data_science_london::model::main();
+    // data_science_london::model::main();
+    icr_identifying_age_related_conditions::model::main();
+    
 }
