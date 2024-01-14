4c38991c1f00f918f12c90333133e69fd2d74ec9
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-01-14 18:24:02 +0900

    feat:drop id

--- src/icr_identifying_age_related_conditions/model.rs
+++ src/icr_identifying_age_related_conditions/model.rs
@@ -7,20 +7,21 @@ pub fn main() {
         CsvReader::from_path("./datasets/icr-identify-age-related-conditions/train.csv")
             .unwrap()
             .finish()
-            .unwrap();
+            .unwrap().drop("Id").unwrap();
 
     let test_df = CsvReader::from_path("./datasets/icr-identify-age-related-conditions/test.csv")
         .unwrap()
         .finish()
-        .unwrap();
+        .unwrap().drop("Id").unwrap();
 
 
     println!("{}", train_df);
+    println!("{:?}",train_df.schema());
 
     /*======processing======= */
 
     /*========result========= */
 
 
-    
+
 }
