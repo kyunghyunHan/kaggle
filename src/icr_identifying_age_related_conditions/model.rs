4d19c9a4f4094e9ba7f8b1ccf5e5853cbe1e4a99
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-01-14 18:16:22 +0900

    feat:test

--- src/icr_identifying_age_related_conditions/model.rs
+++ src/icr_identifying_age_related_conditions/model.rs
@@ -1,13 +1,26 @@
-use polars::prelude::*;
 use plotters::prelude::*;
+use polars::prelude::*;
+
+pub fn main() {
+    /*============data============= */
+    let train_df: DataFrame =
+        CsvReader::from_path("./datasets/icr-identify-age-related-conditions/train.csv")
+            .unwrap()
+            .finish()
+            .unwrap();
+
+    let test_df = CsvReader::from_path("./datasets/icr-identify-age-related-conditions/test.csv")
+        .unwrap()
+        .finish()
+        .unwrap();
 
 
+    println!("{}", train_df);
 
-pub fn main(){
-     let train_df: DataFrame = CsvReader::from_path("./datasets/icr-identify-age-related-conditions/train.csv")
-     .unwrap()
-     .finish()
-     .unwrap();
-    println!("{}",train_df);
+    /*======processing======= */
 
+    /*========result========= */
+
+
+    
 }
