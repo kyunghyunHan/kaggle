ad7dfc7e87ab23622d1aa57540f5006d99aa4483
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-02-07 18:37:31 +0900

    featt:test

--- src/penguins_binary_classification/model.rs
+++ src/penguins_binary_classification/model.rs
@@ -1,64 +1,68 @@
+use burn::data::dataset::{Dataset, InMemDataset};
+use burn::record::Recorder;
+use burn::tensor::ElementConversion;
 use burn::{
+    backend::{wgpu::AutoGraphicsApi, Autodiff, Wgpu},
     config::Config,
+    data::dataloader::{batcher::Batcher, DataLoaderBuilder},
     module::Module,
     nn::{
-        conv::{Conv2d, Conv2dConfig},//기본convolution
+        conv::{Conv2d, Conv2dConfig}, //기본convolution
+        loss::CrossEntropyLoss,
         pool::{AdaptiveAvgPool2d, AdaptiveAvgPool2dConfig},
-        Dropout, DropoutConfig, Linear, LinearConfig, ReLU,
-        loss::CrossEntropyLoss
-    },
-    tensor::{
-        backend::{
-            Backend,AutodiffBackend
-        }, Data, Int, Tensor
-    },
-    train::{
-        ClassificationOutput,TrainStep,TrainOutput,ValidStep,LearnerBuilder,
-        metric::{
-            AccuracyMetric,
-            LossMetric
-        }
+        Dropout,
+        DropoutConfig,
+        Linear,
+        LinearConfig,
+        ReLU,
     },
     optim::AdamConfig,
-    data::dataloader::{DataLoaderBuilder,batcher::Batcher},
     record::CompactRecorder,
-    backend::{
-        Autodiff,
-        Wgpu,
-        wgpu::AutoGraphicsApi
-    }
+    tensor::{
+        backend::{AutodiffBackend, Backend},
+        Data, Int, Tensor,
+    },
+    train::{
+        metric::{AccuracyMetric, LossMetric},
+        ClassificationOutput, LearnerBuilder, TrainOutput, TrainStep, ValidStep,
+    },
 };
-use serde::{Deserialize, Serialize};
-use burn::data::dataset::{Dataset, InMemDataset};
-use std::path::Path;
-use burn::tensor::ElementConversion;
-use burn::record::Recorder;
 use polars::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::path::Path;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct PenguinsData {
-    #[serde(rename = "Width")]
-    pub width:i64,
-
+    pub species: i64,
+    pub island: i64,
+    pub bill_length_mm: i64,
+    pub bill_depth_mm: i64,
+    pub flipper_length_mm: i64,
+    pub body_mass_g: i64,
+    pub year: i64,
 }
 pub struct PenguinsDataset {
     dataset: Vec<PenguinsData>,
 }
 impl PenguinsDataset {
     pub fn new() -> Self {
-        let train_df = CsvReader::from_path("./datasets/digit-recognizer/train.csv")
+        let train_df = CsvReader::from_path("./datasets/digit-penguins_binary_classification/penguins_binary_classification.csv")
             .unwrap()
             .finish()
             .unwrap();
-        let penguins_data= PenguinsData{
-            width:1
+        let penguins_data = PenguinsData {
+            species: 1,
+            island: 1,
+            bill_length_mm: 1,
+            bill_depth_mm: 1,
+            flipper_length_mm: 1,
+            body_mass_g: 1,
+            year: 1,
         };
-        let a= vec![penguins_data];
-        PenguinsDataset { dataset: a}
-
+        let a = vec![penguins_data];
+        PenguinsDataset { dataset: a }
     }
 }
-pub fn main(){
-
-    println!("{}","펭귄");
+pub fn main() {
+    println!("{}", "펭귄");
 }
