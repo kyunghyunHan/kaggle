79c9d69ce1ceadb13ed65cbb255418b7812877f8
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-08 15:39:16 +0900

    feat:setting

--- src/playground_series_s3e24/mod.rs
+++ src/playground_series_s3e24/mod.rs
@@ -1,6 +1,22 @@
 pub mod model{
-    pub fn main()->anyhow::Result<()>{
-        println!("{}",1);
+
+    use candle_core::{Tensor,Result as CRS};
+    use polars::prelude::*;
+    struct  Dataset {
+        x_train:Tensor,
+        y_train:Tensor,
+        x_test:Tensor,
+        y_test:Tensor,
+    }
+    impl Dataset{
+        fn new()->CRS<()>{
+            
         Ok(())
+        }
+
+    }
+    pub fn main(){
+        let  train_df=  CsvReader::from_path("./datasets/playground-series-s3e24/train.csv").unwrap().finish().unwrap();
+        
     }
 }
