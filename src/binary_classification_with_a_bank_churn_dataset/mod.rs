b1a461872e87e341862eb64436fee89a015080a6
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-12 11:09:30 +0900

    feat:null check

--- src/binary_classification_with_a_bank_churn_dataset/mod.rs
+++ src/binary_classification_with_a_bank_churn_dataset/mod.rs
@@ -3,8 +3,28 @@ pub mod model {
     use polars::prelude::*;
     use candle_core::{Tensor,D};
     use std::fs::File;
+    #[derive(Debug)]
+    struct Dataset{
+        x_train:Tensor,
+        y_train:Tensor,
+        x_test:Tensor,
+        y_test:Tensor,
+    }
+    impl Dataset{
+        fn new(){
+            let train_df=CsvReader::from_path("./datasets/playground-series-s4e1/train.csv").unwrap().finish().unwrap();
+            let test_df=CsvReader::from_path("./datasets/playground-series-s4e1/test.csv").unwrap().finish().unwrap();
+            let submussion_df=CsvReader::from_path("./datasets/playground-series-s4e1/sample_submission.csv").unwrap().finish().unwrap();
+            
+            //NUll check
+            println!("{}",train_df.null_count());
+            println!("{}",test_df.null_count());
+            println!("{}",submussion_df.null_count());
 
+        }
+    }
     pub fn main()->anyhow::Result<()> {
+        Dataset::new();
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod binary_classification_with_a_bank_churn_dataset;
 use rayon::prelude::*;
 
 fn main() {
+    binary_classification_with_a_bank_churn_dataset::model::main().unwrap();
     // deep_titanic::model::main().unwrap();
-    playground_series_s3e24::model::main().unwrap();
+    // playground_series_s3e24::model::main().unwrap();
 }
